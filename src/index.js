236b84e0d25a447798fbc29421b8ca2435f28751
Tanney-102 <dhxodms89@gmail.com>
2020-08-26 04:16:08 +0900

    new fetch

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ const $backBtn = document.querySelector('.back-btn');
 
 renderAreaBtns();
 $backBtn.addEventListener('click', renderAreaBtns);
-fetch('https://weather-refugee-camp.netlify.app/areaTree.json', {method:'get'})
-.then(res => { return res.json() })
-.then(data => console.log(data));
 
 //
 //functions
@@ -15,14 +12,15 @@ function renderAreaBtns() {
     $backBtn.style.display = 'none';
     $locSelectArea.innerHTML = '';
 
-    getAreaCode()
-    .then( areas => {
-        areas.forEach(_area => {
-            // _area = [code, name]
-            // name = area name, value = area code
+    fetch('https://weather-refugee-camp.netlify.app/areaTree.json', {method:'get'})
+    .then(res => { return res.json() })
+    .then(areaTree => {
+        const areas = areaTree.area;
+        
+        areas.forEach(name => {
             $locSelectArea.innerHTML += `
-                <button name="${_area[1]}" value="${_area[0]}" class="location-btn btn-hover">
-                    ${_area[1]}
+                <button name="${name}" class="location-btn btn-hover">
+                    ${name}
                 </button>
             `;
         });
@@ -31,78 +29,30 @@ function renderAreaBtns() {
             $locSelectArea.children[i].addEventListener('click', renderSubAreaBtns);
         }
     });
-
-
-}
-
-async function getAreaCode(code) {
-    const key = '8mVPSCRQrFyWbjSf4iKzswPjcu%2FRFGP1stNocEROwnzupvjPfF6MTVT5TSQRCwunx0HAcrMRDghtU0HKFRZumw%3D%3D'
-    const url_base = `http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode?ServiceKey=${key}&MobileOS=ETC&MobileApp=AppTesting&numOfRows=30`
-    const url = getAreaCodeURL(code, url_base);
-    const config = {
-        method: 'get',
-    }
-
-    const areas = await fetch(url, config)
-    .then(res => res.text())
-    .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
-    .then(doc => {
-        const items = doc.getElementsByTagName('item');
-        const areas = [];
-        for(let i=0; i<items.length; i++) {
-            const _area = [items[i].children[0].innerHTML, items[i].children[1].innerHTML];
-            areas.push(_area);
-        }
-
-        return areas;
-    })
-    .catch(err => console.error(err));
-
-    return areas;
-}
-
-function getAreaCodeURL(code, url_base) {
-    if(code)
-        return url_base + '&areaCode=' + code;
-    return url_base;
 }
 
 function renderSubAreaBtns(e) {
-    const parentArea = {
-        'name' : e.target.name,
-        'code' : e.target.value,
-    };
+    const parentName = e.target.name;
     
-    $locHeader.innerHTML = parentArea.name;
+    $locHeader.innerHTML = parentName;
     $backBtn.style.display = 'block';
     $locSelectArea.innerHTML = ''
 
-    getAreaCode(parentArea.code)
-    .then( areas => {
-        areas.forEach(_area => {
-            const url = encodeURI(`./weather.html?parentName=${parentArea.name}&childName=${_area[1]}`);
+    fetch('https://weather-refugee-camp.netlify.app/areaTree.json', {method:'get'})
+    .then(res => { return res.json() })
+    .then(areaTree => {
+        const areas = areaTree.subArea[parentName];
 
-            // _area = [code, name]
-            // name = area name, value = area code
+        areas.forEach(name => {
+            const url = encodeURI(`./weather.html?parentName=${parentName}&childName=${name}`);
+
             $locSelectArea.innerHTML += `
-                <button name="${_area[1]}" value="${_area[0]}" class="location-btn btn-hover">
+                <button class="location-btn btn-hover">
                     <a href="${url}" class="a-in-btn">
-                        ${_area[1]}
+                        ${name}
                     </a>
                 </button>
             `;
         });
-
-        // for(let i=0; i<areas.length; i++) {
-        //     $locSelectArea.children[i].addEventListener('click', (e) => {
-        //         loadWeatherPage(e, parentArea);
-        //     });
-        // }
     });
-}
-
-function loadWeatherPage(e, parentArea) {
-    const url = `./weather.html?parentName=${parentArea.name}&childName=${e.target.name}`;
-    
-    location.replace(url);
 }
